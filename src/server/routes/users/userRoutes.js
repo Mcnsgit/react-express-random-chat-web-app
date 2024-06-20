3c87f93f39e27168b5fffa21d4480acc845f4ee6
mcnsgit <cardigamiguel221@gmail.com>
2024-06-20 01:22:08 +0100

    delete 12320 files

--- src/server/routes/users/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const matchController = require('../controllers/matchController');
-const { protect } = require('../middleware/authMiddleware'); // Assuming you have an auth middleware
-
-router.post('/find-match', protect, matchController.findMatchForUser);
-
-module.exports = router;
